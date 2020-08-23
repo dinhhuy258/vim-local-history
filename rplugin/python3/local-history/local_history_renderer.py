c60e23c20ba396575170a97a2c57a28aed903ee9
Huy Duong <huy.duongdinh@gmail.com>
2020-08-24 00:49:20 +0700

    Set local history filetype

--- rplugin/python3/local-history/local_history_renderer.py
+++ rplugin/python3/local-history/local_history_renderer.py
@@ -4,9 +4,13 @@ from pynvim.api.window import Window
 from .settings import Settings
 from .logging import log
 from .nvim import call_nvim, get_current_buffer_name, is_buf_visilbe, create_buffer, create_window, win_set_buf
+
+_LOCAL_HISTORY_BUF_FILE_TYPE = 'LocalHistory'
+
+
 async def local_history_toggle(nvim: Nvim, settings: Settings) -> None:
     def func() -> None:
-        buffer = create_buffer(nvim)
+        buffer = create_buffer(nvim, _LOCAL_HISTORY_BUF_FILE_TYPE)
         window = create_window(nvim, 30)
         win_set_buf(nvim, window, buffer)
 
--- rplugin/python3/local-history/nvim.py
+++ rplugin/python3/local-history/nvim.py
@@ -23,26 +23,32 @@ def call_nvim(nvim: Nvim, func: Callable[[], T]) -> Awaitable[T]:
     nvim.async_call(run)
     return future
 
+
 def get_current_buffer_name(nvim: Nvim) -> str:
     buffer = nvim.api.get_current_buf()
     buffer_name = nvim.api.buf_get_name(buffer)
 
     return buffer_name
 
+
 def is_buf_visilbe(nvim: Nvim, buf_name: str) -> bool:
     return int(nvim.api.eval(bufwinnr(bufnr(buf_name)))) != -1
+
 
 def get_current_win() -> Window:
     return nvim.api.get_current_win()
 
-def create_buffer(nvim: Nvim) -> Buffer:
+
+def create_buffer(nvim: Nvim, file_type: str) -> Buffer:
     options = {"noremap": True, "silent": True, "nowait": True}
     buffer: Buffer = nvim.api.create_buf(False, True)
     nvim.api.buf_set_option(buffer, "modifiable", False)
+    nvim.api.buf_set_option(buffer, "filetype", file_type)
 
     return buffer
 
-def find_windows_in_tab(nvim: Nvim, exclude: bool) -> Iterator[Window]:
+
+def find_windows_in_tab(nvim: Nvim) -> Iterator[Window]:
     def key_by(window: Window) -> Tuple[int, int]:
         row, col = nvim.api.win_get_position(window)
         return (col, row)
@@ -51,16 +57,16 @@ def find_windows_in_tab(nvim: Nvim, exclude: bool) -> Iterator[Window]:
     windows: Sequence[Window] = nvim.api.tabpage_list_wins(tab)
 
     for window in sorted(windows, key=key_by):
-        if not exclude or not nvim.api.win_get_option(window, "previewwindow"):
+        if not nvim.api.win_get_option(window, "previewwindow"):
             yield window
+
 
 def create_window(nvim: Nvim, width: int) -> Window:
     split_right = nvim.api.get_option("splitright")
 
-    # TODO: Get first window no need to
-    windows: Sequence[Window] = tuple(
-        window for window in find_windows_in_tab(nvim, exclude=False)
-    )
+    windows: Sequence[Window] = tuple(window
+                                      for window in find_windows_in_tab(nvim))
+
     focus_win = windows[0]
 
     nvim.api.set_option("splitright", False)
@@ -71,8 +77,10 @@ def create_window(nvim: Nvim, width: int) -> Window:
     window: Window = nvim.api.get_current_win()
     return window
 
+
 def win_set_buf(nvim: Nvim, window: Window, buffer: Buffer) -> None:
     nvim.api.win_set_buf(window, buffer)
+
 
 def command(cmd: str) -> None:
     nvim.api.command(cmd)
